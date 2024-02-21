30038a6cefa1579ba185bfe4916d6d7bceedec1c
abdullah201240 <abdullahalsakib7075@gmail.com>
2024-02-21 15:15:06 +0600

    signup

--- App.tsx
+++ App.tsx
@@ -80,171 +80,187 @@
 //   },
 // });
 
-import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Button, View, Image, Text, TouchableOpacity } from 'react-native';
-import { Camera as ExpoCamera } from 'expo-camera';
-import * as DocumentPicker from 'expo-document-picker';
-import axios from 'axios';
+// import React, { useState, useEffect, useRef } from 'react';
+// import { StyleSheet, Button, View, Image, Text, TouchableOpacity } from 'react-native';
+// import { Camera as ExpoCamera } from 'expo-camera';
+// import * as DocumentPicker from 'expo-document-picker';
+// import axios from 'axios';
 
-export default function App() {
-  const apiUrl = 'http://192.168.0.110:8080/upload';
-  const [image, setImage] = useState('');
-  const [type, setType] = useState('back' as 'back' | 'front');
-  const [hasCameraPermission, setHasCameraPermission] = useState<boolean | null>(null);
-  const [isCameraReady, setCameraReady] = useState(false);
+// export default function App() {
+//   const apiUrl = 'http://192.168.0.110:8080/upload';
+//   const [image, setImage] = useState('');
+//   const [type, setType] = useState('back' as 'back' | 'front');
+//   const [hasCameraPermission, setHasCameraPermission] = useState<boolean | null>(null);
+//   const [isCameraReady, setCameraReady] = useState(false);
 
-  const cameraRef = useRef<any>(null);
+//   const cameraRef = useRef<any>(null);
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await ExpoCamera.requestCameraPermissionsAsync();
-      setHasCameraPermission(status === 'granted');
-    })();
-  }, []);
+//   useEffect(() => {
+//     (async () => {
+//       const { status } = await ExpoCamera.requestCameraPermissionsAsync();
+//       setHasCameraPermission(status === 'granted');
+//     })();
+//   }, []);
 
-  if (hasCameraPermission === null) {
-    return <Text>Requesting camera permission...</Text>;
-  }
+//   if (hasCameraPermission === null) {
+//     return <Text>Requesting camera permission...</Text>;
+//   }
 
-  if (hasCameraPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
+//   if (hasCameraPermission === false) {
+//     return <Text>No access to camera</Text>;
+//   }
 
-  const pickSomething = async () => {
-    try {
-      const docRes = await DocumentPicker.getDocumentAsync({
-        type: 'image/*',
-      });
+//   const pickSomething = async () => {
+//     try {
+//       const docRes = await DocumentPicker.getDocumentAsync({
+//         type: 'image/*',
+//       });
 
-      const formData = new FormData();
-      const assets = docRes.assets;
+//       const formData = new FormData();
+//       const assets = docRes.assets;
 
-      if (!assets) return;
+//       if (!assets) return;
 
-      const file = assets[0];
+//       const file = assets[0];
 
-      const imageFile = {
-        name: file.name.split('.')[0],
-        uri: file.uri,
-        type: file.mimeType,
-        size: file.size,
-      };
+//       const imageFile = {
+//         name: file.name.split('.')[0],
+//         uri: file.uri,
+//         type: file.mimeType,
+//         size: file.size,
+//       };
 
-      formData.append('image', imageFile as any);
+//       formData.append('image', imageFile as any);
 
-      const axiosConfig = {
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'multipart/form-data',
-        },
-      };
+//       const axiosConfig = {
+//         headers: {
+//           Accept: 'application/json',
+//           'Content-Type': 'multipart/form-data',
+//         },
+//       };
 
-      const response = await axios.post(apiUrl, formData, axiosConfig);
+//       const response = await axios.post(apiUrl, formData, axiosConfig);
 
-      setImage(response.data.image);
-    } catch (error) {
-      console.error('Error while selecting file: ', error);
-    }
-  };
+//       setImage(response.data.image);
+//     } catch (error) {
+//       console.error('Error while selecting file: ', error);
+//     }
+//   };
 
-  const handleCameraCapture = async () => {
-    if (!cameraRef.current || !isCameraReady) {
-      console.warn('Camera is not ready yet. Wait for onCameraReady callback.');
-      return;
-    }
+//   const handleCameraCapture = async () => {
+//     if (!cameraRef.current || !isCameraReady) {
+//       console.warn('Camera is not ready yet. Wait for onCameraReady callback.');
+//       return;
+//     }
 
-    try {
-      const photo = await (cameraRef.current as any).takePictureAsync({ base64: true });
-      const formData = new FormData();
-      const imageFile = {
-        name: 'photo.jpg',
-        type: 'image/jpeg',
-        uri: photo.uri,
-      };
+//     try {
+//       const photo = await (cameraRef.current as any).takePictureAsync({ base64: true });
+//       const formData = new FormData();
+//       const imageFile = {
+//         name: 'photo.jpg',
+//         type: 'image/jpeg',
+//         uri: photo.uri,
+//       };
 
-      formData.append('image', imageFile as any);
+//       formData.append('image', imageFile as any);
 
-      const axiosConfig = {
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'multipart/form-data',
-        },
-      };
+//       const axiosConfig = {
+//         headers: {
+//           Accept: 'application/json',
+//           'Content-Type': 'multipart/form-data',
+//         },
+//       };
 
-      const response = await axios.post(apiUrl, formData, axiosConfig);
-      setImage(response.data.image);
-    } catch (error) {
-      console.error('Error while taking photo: ', error);
-    }
-  };
+//       const response = await axios.post(apiUrl, formData, axiosConfig);
+//       setImage(response.data.image);
+//     } catch (error) {
+//       console.error('Error while taking photo: ', error);
+//     }
+//   };
 
-  const toggleCameraType = () => {
-    setType((current: "back" | "front") =>
-      current === "back" ? "front" : "back"
-    );
-  };
+//   const toggleCameraType = () => {
+//     setType((current: "back" | "front") =>
+//       current === "back" ? "front" : "back"
+//     );
+//   };
 
+//   return (
+//     <View style={styles.container}>
+//       <ExpoCamera
+//         style={styles.camera}
+//         type={type as any}
+
+//         onCameraReady={() => setCameraReady(true)}
+//         ref={(ref) => (cameraRef.current = ref!)}
+//         >
+//         <View style={styles.cameraContainer}>
+//           <TouchableOpacity style={styles.flipCameraButton} onPress={toggleCameraType}>
+//             <Text style={styles.flipCameraButtonText}>Flip Camera</Text>
+//           </TouchableOpacity>
+//         </View>
+//       </ExpoCamera>
+
+//       <View style={styles.buttonContainer}>
+//         <Button title="Pick something" onPress={pickSomething} />
+//         <Button title="Take Photo" onPress={handleCameraCapture} />
+//       </View>
+//       {image && <Image source={{ uri: `data:image/jpeg;base64,${image}` }} style={styles.image} />}
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   camera: {
+//     flex: 1,
+//     width: '100%',
+//   },
+//   cameraContainer: {
+//     flex: 1,
+//     backgroundColor: 'transparent',
+//     flexDirection: 'row',
+//     justifyContent: 'flex-end',
+//     margin: 20,
+//   },
+//   flipCameraButton: {
+//     alignSelf: 'flex-end',
+//     alignItems: 'center',
+//     backgroundColor: 'transparent',
+//     padding: 15,
+//   },
+//   flipCameraButtonText: {
+//     color: '#fff',
+//   },
+//   buttonContainer: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-around',
+//     marginTop: 20,
+//   },
+//   image: {
+//     width: 200,
+//     height: 200,
+//     marginTop: 20,
+//   },
+// });
+
+import {  View } from 'react-native'
+import React from 'react'
+import Signup from './components/signup'
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <ExpoCamera
-        style={styles.camera}
-        type={type as any}
-
-        onCameraReady={() => setCameraReady(true)}
-        ref={(ref) => (cameraRef.current = ref!)}
-        >
-        <View style={styles.cameraContainer}>
-          <TouchableOpacity style={styles.flipCameraButton} onPress={toggleCameraType}>
-            <Text style={styles.flipCameraButtonText}>Flip Camera</Text>
-          </TouchableOpacity>
-        </View>
-      </ExpoCamera>
-
-      <View style={styles.buttonContainer}>
-        <Button title="Pick something" onPress={pickSomething} />
-        <Button title="Take Photo" onPress={handleCameraCapture} />
-      </View>
-      {image && <Image source={{ uri: `data:image/jpeg;base64,${image}` }} style={styles.image} />}
+    <View>
+      <Signup/>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  camera: {
-    flex: 1,
-    width: '100%',
-  },
-  cameraContainer: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    margin: 20,
-  },
-  flipCameraButton: {
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-    padding: 15,
-  },
-  flipCameraButtonText: {
-    color: '#fff',
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginTop: 20,
-  },
-  image: {
-    width: 200,
-    height: 200,
-    marginTop: 20,
-  },
-});
+export default App
+
+
 
--- components/signup.tsx
+++ components/signup.tsx
@@ -1,20 +1,137 @@
-
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet, Text, Image, View, TextInput, TouchableOpacity, Alert,Pressable} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import axios from "axios";
 
 const Signup = () => {
-  return (
-    <View>
-      <Text>signup</Text>
-      
-    </View>
-  )
-}
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const handleSignup = async () => {
+        try {
+            if (!name || !email || !password) {
+                Alert.alert("Validation Error", "Please fill in all fields");
+                return;
+            }
+    
+            const apiEndpoint = "http://192.168.0.110:8080/signup";
+    
+            const response = await axios.post(apiEndpoint, {
+                name: name,
+                email: email,
+                password: password,
+            });
+    
+            console.log("API Response:", response.data);
+    
+            Alert.alert("Success", "User created successfully");
+        } catch (error:any) {
+            if (error.response && error.response.status === 400) {
+                const errorMessage = error.response.data.error || "";
+                if (errorMessage.includes("User with this email already exists")) {
+                    Alert.alert("Error", "User with this email already exists. Please use a different email.");
+                } else {
+                    Alert.alert("Error", errorMessage || "Failed to create user");
+                }
+            } else {
+                Alert.alert("Error", "Failed to create user");
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
+    
+    
+   
 
-export default Signup
+
+    return (
+        <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+            <Image source={require("../assets/logo.png")} style={styles.logo} />
+            
+            <Text style={{ fontSize: 30, color: "green", fontWeight: "bold" }}>
+               ObjectSnap       
+                    </Text>
+            <Text style={{ fontSize: 30, color: "rgb(88, 14, 206)", fontWeight: "bold" }}>
+                Create Account
+            </Text>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Name"
+                    value={name}
+                    onChangeText={(text) => setName(text)}
+                />
+
+                <TextInput
+                    style={styles.input}
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Password"
+                    secureTextEntry
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                />
+            </View>
+            <TouchableOpacity style={styles.buttonContainer} onPress={handleSignup}>
+                <Text style={styles.buttonText}>Sign Up</Text>
+            </TouchableOpacity>
+            {/* <Pressable onPress={() => navigation.navigate("Signin")}>
+
+        <Text style={{ paddingTop: 10 ,fontSize:18}}>Have an account? <Text style={{ color: "violet",fontSize:18 }}>Sign In</Text></Text>
+
+
+
+      </Pressable> */}
+
+            <StatusBar style="auto" />
+        </ScrollView>
+    );
+};
+
+export default Signup;
 
 const styles = StyleSheet.create({
+    scrollViewContainer: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop:50,
 
-
-
-})
+    },
+    logo: {
+        width: 200,
+        height: 200,
+        resizeMode: 'contain',
+    },
+    inputContainer: {
+        marginTop: 20,
+        width: 300,
+    },
+    input: {
+        height: 40,
+        borderColor: "gray",
+        borderWidth: 1,
+        marginBottom: 10,
+        paddingHorizontal: 10,
+        borderRadius: 8,
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 18,
+    },
+    buttonContainer: {
+        marginTop: 10,
+        height: 50,
+        width: 200,
+        backgroundColor: "rgb(85, 12, 206)",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 8,
+    },
+});
